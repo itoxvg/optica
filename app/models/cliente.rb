5243a920236c0779bb6e7eaf2cd531982197e7a4
iTox <juanthoniovg@gmail.com>
2019-02-20 07:51:45 -0600

    se quitó la validación de único cliente

--- app/models/cliente.rb
+++ app/models/cliente.rb
@@ -2,7 +2,6 @@ class Cliente < ApplicationRecord
   has_many :ventas
 
   validates :nombre, presence: true
-  validates :nombre, uniqueness: { case_sensitive: false }
 
   def to_s
     nombre
--- spec/models/cliente_spec.rb
+++ spec/models/cliente_spec.rb
@@ -4,5 +4,4 @@ RSpec.describe Cliente, type: :model do
   it { should have_many :ventas }
 
   it { should validate_presence_of :nombre }
-  it { should validate_uniqueness_of(:nombre).case_insensitive }
 end
