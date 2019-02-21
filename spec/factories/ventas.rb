08ba58f3734ef697b95c6162a52f19965e187d4b
iTox <juanthoniovg@gmail.com>
2019-02-21 07:59:11 -0600

    se agregó suma_precios_venta al modelo venta
    suma el precio de venta de los productos vendidos

--- spec/factories/ventas.rb
+++ spec/factories/ventas.rb
@@ -10,4 +10,24 @@ FactoryBot.define do
     cliente
     association :usuario, factory: :vendedor
   end
+
+  trait :con_dos_armazones do
+    after :create do |venta|
+      armazon = create :armazon
+
+      create :vendido, precio_venta: 250, precio_compra: 50,
+        cantidad: 2, descuento: 50, subtotal: 450,
+        venta: venta, producto: armazon
+    end
+  end
+
+  trait :con_dos_micas do
+    after :create do |venta|
+      mica = create :mica
+
+      create :vendido, precio_venta: 200, precio_compra: 50,
+        cantidad: 2, descuento: 50, subtotal: 350,
+        venta: venta, producto: mica
+    end
+  end
 end
