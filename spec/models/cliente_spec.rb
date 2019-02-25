b4d0ffb91df8ee9da8813f9088c665c1cf2a9ec7
Juan Vásquez <juanthoniovg@gmail.com>
2019-02-24 21:05:26 -0600

    se agregó validación 'nombre' unicó para cliente

--- spec/models/cliente_spec.rb
+++ spec/models/cliente_spec.rb
@@ -4,4 +4,5 @@ RSpec.describe Cliente, type: :model do
   it { should have_many :ventas }
 
   it { should validate_presence_of :nombre }
+  it { should validate_uniqueness_of(:nombre).case_insensitive }
 end
