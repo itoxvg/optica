27f63b14e3bb9d6f95d51675f92e56e202fbb4e8
Juan Vásquez <juanthoniovg@gmail.com>
2019-02-26 15:02:17 -0600

    se agregó una imagen para el inicio de sesión

--- app/javascript/assets/main.js
+++ app/javascript/assets/main.js
@@ -15,5 +15,7 @@ import 'assets/css/codebase.min.css'
 import 'assets/js/codebase.js'
 import 'assets/js/plugins'
 
+import 'assets/css/optica.css'
+
 const images = require.context('assets/images', true)
 const imagePath = (name) => images(name, true)
