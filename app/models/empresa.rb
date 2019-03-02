e64a2b1175d7e42f4ff09e98c8b9e546c534e6e5
Juan Vásquez <juanthoniovg@gmail.com>
2019-03-02 17:58:54 -0600

    se agregó accept_nested_attributes_for domicilio en empresa

--- app/models/empresa.rb
+++ app/models/empresa.rb
@@ -2,6 +2,7 @@ class Empresa < ApplicationRecord
   validates :nombre, presence: true
 
   has_one :domicilio, as: :domiciliable, dependent: :destroy
+  accepts_nested_attributes_for :domicilio, allow_destroy: true
 
   def to_s
     nombre
--- spec/models/empresa_spec.rb
+++ spec/models/empresa_spec.rb
@@ -4,4 +4,5 @@ RSpec.describe Empresa, type: :model do
   it { should validate_presence_of :nombre }
 
   it { should have_one(:domicilio).dependent(:destroy) }
+  it { should accept_nested_attributes_for(:domicilio).allow_destroy(true) }
 end
