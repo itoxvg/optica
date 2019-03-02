d5ae225e013ef748ecb619ecfb97003c1599b816
Juan Vásquez <juanthoniovg@gmail.com>
2019-03-01 20:51:00 -0600

    se cambió baseURL para axios

--- app/javascript/packs/pages.js
+++ app/javascript/packs/pages.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import axios from 'axios'
 
-axios.defaults.baseURL = 'http://localhost:3000'
+axios.defaults.baseURL = window.location.origin
 axios.defaults.headers.post['Content-Type'] = 'application/json'
 axios.defaults.headers.post['Accept'] = 'application/json'
 axios.defaults.headers.common['X-CSRF-Token'] = document.querySelector('meta[name="csrf-token"]').getAttribute('content')
