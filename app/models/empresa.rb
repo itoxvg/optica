6bd86eaf78585022fbd4778049fc6ac65fe4c9a5
Juan Vásquez <juanthoniovg@gmail.com>
2019-03-02 17:24:03 -0600

    se agregó la relación empresa domicilio

--- app/models/empresa.rb
+++ app/models/empresa.rb
@@ -1,6 +1,8 @@
 class Empresa < ApplicationRecord
   validates :nombre, presence: true
 
+  has_one :domicilio, as: :domiciliable, dependent: :destroy
+
   def to_s
     nombre
   end
--- spec/models/empresa_spec.rb
+++ spec/models/empresa_spec.rb
@@ -2,4 +2,6 @@ require 'rails_helper'
 
 RSpec.describe Empresa, type: :model do
   it { should validate_presence_of :nombre }
+
+  it { should have_one(:domicilio).dependent(:destroy) }
 end
