5b4bcbe037943d0564d65e92ee8949483b5be702
Juan Vásquez <juanthoniovg@gmail.com>
2019-03-02 18:13:43 -0600

    se delegó los atributos de domicilio a empresa

--- app/models/empresa.rb
+++ app/models/empresa.rb
@@ -4,6 +4,9 @@ class Empresa < ApplicationRecord
   has_one :domicilio, as: :domiciliable, dependent: :destroy
   accepts_nested_attributes_for :domicilio, allow_destroy: true
 
+  delegate :calle, :numero, :colonia, :codigo_postal, :municipio, :estado,
+    :pais, to: :domicilio, prefix: false, allow_nil: true
+
   def to_s
     nombre
   end
