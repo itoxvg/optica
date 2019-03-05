bba5447378c95f652671baee6d354fa4a2168b56
Juan Vásquez <juanthoniovg@gmail.com>
2019-03-04 23:42:14 -0600

    se agregó to_s para domicilio

--- app/models/domicilio.rb
+++ app/models/domicilio.rb
@@ -2,4 +2,8 @@ class Domicilio < ApplicationRecord
   belongs_to :domiciliable, polymorphic: true
 
   validates :pais, :estado, presence: true
+
+  def to_s
+    "#{calle} ##{numero} \n #{colonia} #{codigo_postal} \n #{municipio}, #{estado}"
+  end
 end
