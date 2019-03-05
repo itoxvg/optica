89268b76938225877134bf5c57ef3b24df0ff1ee
Juan Vásquez <juanthoniovg@gmail.com>
2019-03-04 23:43:02 -0600

    se cambió el formato de boleto de venta

--- spec/requests/api/boletos_spec.rb
+++ spec/requests/api/boletos_spec.rb
@@ -2,6 +2,7 @@ require 'rails_helper'
 
 RSpec.describe "Boletos", type: :request do
 
+  let!(:empresa) { create :empresa }
   let(:vendedor) { create :vendedor }
 
   before :each do
@@ -11,7 +12,7 @@ RSpec.describe "Boletos", type: :request do
   describe "GET /api/boletos/:id/ventas" do
     let(:venta) { create :venta, usuario: vendedor }
 
-    it "returns a venta's boleto" do
+    it "debe regresar el boleto de venta" do
       get ventas_api_boleto_path venta
       expect(response).to have_http_status 200
     end
