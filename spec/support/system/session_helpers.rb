5e991361a3e633a562909968978a25381181fd17
Juan Vásquez <juanthoniovg@gmail.com>
2019-03-06 14:59:00 -0600

    se agregaron helpers para pruebas de system

---
+++ spec/support/system/session_helpers.rb
@@ -0,0 +1,42 @@
+module System
+  module SessionHelpers
+
+    def cuando_vendedor_inicia_sesion(vendedor=crear_vendedor)
+      cuando_visita_el_inicio_de_sesion
+      cuando_ingresa_credenciales vendedor
+      cuando_hace_click_en_el_boton_iniciar_sesion
+      espero_ver_el_nombre_de_usuario
+    end
+
+    def crear_vendedor
+      create :vendedor
+    end
+
+    def cuando_admin_inicia_sesion(admin=crear_admin)
+      cuando_visita_el_inicio_de_sesion
+      cuando_ingresa_credenciales admin
+      cuando_hace_click_en_el_boton_iniciar_sesion
+      espero_ver_el_nombre_de_usuario
+    end
+
+    def crear_admin
+      create :admin
+    end
+
+    def cuando_ingresa_credenciales usuario
+      within('#new_usuario') do
+        fill_in 'usuario_email', with: usuario.email
+        fill_in 'usuario_password', with: usuario.password
+      end
+    end
+
+    def cuando_hace_click_en_el_boton_iniciar_sesion
+      click_button 'iniciar sesión'
+    end
+
+  end
+end
+
+RSpec.configure do |config|
+  config.include System::SessionHelpers, type: :system
+end
