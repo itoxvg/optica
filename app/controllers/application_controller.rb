ab47addc13b5d1e87ac8f70b04e66e318d2b4f3f
Juan Vásquez <juanthoniovg@gmail.com>
2019-03-10 08:20:29 -0600

    se agregó la configuración de pundit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,9 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_usuario!
   layout :establecer_plantilla
 
+  include Pundit
+  protect_from_forgery
+
   private
 
   def establecer_plantilla
