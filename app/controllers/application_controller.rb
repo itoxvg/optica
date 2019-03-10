ca4eaaf4493920f4710a2b2aa8ffb2e0990495e0
Juan Vásquez <juanthoniovg@gmail.com>
2019-03-10 08:23:05 -0600

    se agregó usuario no autorizado rescue

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,8 +4,17 @@ class ApplicationController < ActionController::Base
 
   include Pundit
   protect_from_forgery
+  rescue_from Pundit::NotAuthorizedError, with: :usuario_no_autorizado
 
   private
+
+  def usuario_no_autorizado exception
+    policy_name = exception.policy.class.to_s.underscore
+
+    flash[:alert] = t "#{policy_name}.#{exception.query}", scope: "pundit",
+                      default: :default
+    redirect_to(request.referrer || root_path)
+  end
 
   def establecer_plantilla
     return 'devise' if devise_controller?
