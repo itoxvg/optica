24f2e48aa485b8d75a4d8d3387c7d8d16f9de210
Juan Vásquez <juanthoniovg@gmail.com>
2019-03-11 10:33:26 -0600

    se agregó marca a los seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,60 +12,62 @@ admin = Administrador.create! nombre: 'juan', telefono: '951 123 45 67',
 Vendedor.create! nombre: 'vendedor', telefono: '951 123 45 67',
   email: 'vendedor@gmail.com', password: '123123123'
 
+marca = Marca.create! nombre: 'OAKLEY'
+
 50.times do |n|
   tipo = ['ranurado', 'armazón completo', 'sobrepuesto'].sample
   Armazon.create! nombre: "Armazon#{n}", codigo: "AZ#{n}",
     precio_venta: rand(500..900), precio_compra: rand(1..400),
-    tipo: tipo, usuario: admin
+    tipo: tipo, usuario: admin, marca_id: marca.id
 end
 
 50.times do |n|
   tipo = ['polarizado', 'filtro uv'].sample
   Lente.create! nombre: "Lente-#{n}", codigo: "LENTE #{n}",
     precio_venta: rand(500..900), precio_compra: rand(1..400),
-    tipo: tipo, usuario: admin
+    tipo: tipo, usuario: admin, marca_id: marca.id
 end
 
 Mica.create! nombre: 'Alta-Force', codigo: 'ALTA FORCE',
   precio_venta: rand(500..900), precio_compra: rand(1..400),
-  existencia: 999999999, usuario: admin
+  existencia: 999999999, usuario: admin, marca_id: marca.id
 Mica.create! nombre: 'Anti-Bluee', codigo: 'ANTI BLUEE',
   precio_venta: rand(500..900), precio_compra: rand(1..400),
-  existencia: 999999999, usuario: admin
+  existencia: 999999999, usuario: admin, marca_id: marca.id
 Mica.create! nombre: 'Blande', codigo: 'BLANDE',
   precio_venta: rand(500..900), precio_compra: rand(1..400),
-  existencia: 999999999, usuario: admin
+  existencia: 999999999, usuario: admin, marca_id: marca.id
 Mica.create! nombre: 'Cr-39-w', codigo: 'CR 39 W',
   precio_venta: rand(500..900), precio_compra: rand(1..400),
-  existencia: 999999999, usuario: admin
+  existencia: 999999999, usuario: admin, marca_id: marca.id
 Mica.create! nombre: 'Flat-Top', codigo: 'FLAT TOP',
   precio_venta: rand(500..900), precio_compra: rand(1..400),
-  existencia: 999999999, usuario: admin
+  existencia: 999999999, usuario: admin, marca_id: marca.id
 Mica.create! nombre: 'Hi-Index', codigo: 'HI INDEX',
   precio_venta: rand(500..900), precio_compra: rand(1..400),
-  existencia: 999999999, usuario: admin
+  existencia: 999999999, usuario: admin, marca_id: marca.id
 Mica.create! nombre: 'Photocromatico', codigo: 'PHOTOCROMATICO',
   precio_venta: rand(500..900), precio_compra: rand(1..400),
-  existencia: 999999999, usuario: admin
+  existencia: 999999999, usuario: admin, marca_id: marca.id
 Mica.create! nombre: 'Polarizado', codigo: 'POLARIZADO',
   precio_venta: rand(500..900), precio_compra: rand(1..400),
-  existencia: 999999999, usuario: admin
+  existencia: 999999999, usuario: admin, marca_id: marca.id
 Mica.create! nombre: 'Policarbonato', codigo: 'POLICARBONATO',
   precio_venta: rand(500..900), precio_compra: rand(1..400),
-  existencia: 999999999, usuario: admin
+  existencia: 999999999, usuario: admin, marca_id: marca.id
 Mica.create! nombre: 'Progresivo', codigo: 'PROGRESIVO',
   precio_venta: rand(500..900), precio_compra: rand(1..400),
-  existencia: 999999999, usuario: admin
+  existencia: 999999999, usuario: admin, marca_id: marca.id
 Mica.create! nombre: 'Transition', codigo: 'TRANSITION',
   precio_venta: rand(500..900), precio_compra: rand(1..400),
-  existencia: 999999999, usuario: admin
+  existencia: 999999999, usuario: admin, marca_id: marca.id
 
 Tratamiento.create! nombre: 'Antireflejante', codigo: 'ANTIREFLEJANTE',
   precio_venta: rand(500..900), precio_compra: rand(1..400),
-  existencia: 999999999, usuario: admin
+  existencia: 999999999, usuario: admin, marca_id: marca.id
 Tratamiento.create! nombre: 'Tinte azúl', codigo: 'TINTE AZÚL',
   precio_venta: rand(500..900), precio_compra: rand(1..400),
-  existencia: 999999999, usuario: admin
+  existencia: 999999999, usuario: admin, marca_id: marca.id
 Tratamiento.create! nombre: 'Tinte café', codigo: 'TINTE CAFÉ',
   precio_venta: rand(500..900), precio_compra: rand(1..400),
-  existencia: 999999999, usuario: admin
+  existencia: 999999999, usuario: admin, marca_id: marca.id
