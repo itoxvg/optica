19bb40e2177d0828b56708b9b81fec9f581cdeb2
Juan Vásquez <juanthoniovg@gmail.com>
2019-03-18 12:39:48 -0600

    se corrigieron pruebas de paginación

--- spec/views/corporaciones/index.html.haml_spec.rb
+++ spec/views/corporaciones/index.html.haml_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe "corporaciones/index", type: :view do
   before(:each) do
-    assign(:corporaciones, [
+    assign(:corporaciones, Kaminari.paginate_array([
       Corporacion.create!(
         nombre: "Nombre",
         rfc: "Rfc",
@@ -15,7 +15,7 @@ RSpec.describe "corporaciones/index", type: :view do
         eslogan: "Eslogan",
         logotipo: FactoryBotHelpers.subir_archivo(nombre: 'opticajj.jpg', formato: 'jpg')
       )
-    ])
+    ]).page(1))
   end
 
   it "muestra la lista de corporaciones" do
--- spec/views/tiendas/index.html.haml_spec.rb
+++ spec/views/tiendas/index.html.haml_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe "tiendas/index", type: :view do
   before(:each) do
-    assign(:tiendas, [
+    assign(:tiendas, Kaminari.paginate_array([
       Tienda.create!(
         nombre: "Nombre",
         rfc: "Rfc",
@@ -15,7 +15,7 @@ RSpec.describe "tiendas/index", type: :view do
         eslogan: "Eslogan",
         logotipo: FactoryBotHelpers.subir_archivo(nombre: 'opticajj.jpg', formato: 'jpg')
       )
-    ])
+    ]).page(1))
   end
 
   it "muestra la lista de tiendas" do
